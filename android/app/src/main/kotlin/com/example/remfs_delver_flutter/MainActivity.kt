9acbbfe7e94f1f08ffcc86e275960d28883a6644
Anders Pitman <tapitman11@gmail.com>
2020-05-06 13:27:17 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/remfs_delver_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.remfs_delver_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "remfs_delver_flutter";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
