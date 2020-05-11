b107a6e480203379063a8837ad219be46e6e0a4a
Anders Pitman <tapitman11@gmail.com>
2020-05-11 13:34:20 -0600

    Implement basic text and image preview pages

--- lib/directory.dart
+++ lib/directory.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'remfs.dart';
+import 'package:mime/mime.dart';
 
 
 class Directory extends StatelessWidget {
@@ -28,7 +29,7 @@ class Directory extends StatelessWidget {
                 return item.value.type == 'dir' ?
                   DirectoryListItem(path: path, name: item.key, remfs: item.value)
                   :
-                  FileListItem(name: item.key, remfs: item.value);
+                  FileListItem(path: path, name: item.key, remfs: item.value);
               }).toList();
 
               //return Text("Hi there");
@@ -58,15 +59,27 @@ class DirectoryListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.folder),
-      title: Text(name),
+    return InkWell(
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Icon(Icons.folder),
+          ),
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => Directory(path: path + '/' + name)),
         );
-      }
+      },
     );
   }
 }
@@ -74,19 +87,86 @@ class DirectoryListItem extends StatelessWidget {
 
 class FileListItem extends StatelessWidget {
 
+  final String path;
   final String name;
   final RemFS remfs;
 
-  FileListItem({this.name, this.remfs});
+  FileListItem({this.path, this.name, this.remfs});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.insert_drive_file),
-      title: Text(name),
+    return InkWell(
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Icon(Icons.insert_drive_file),
+          ),
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
       onTap: () {
-        print("file item");
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FilePreview(path: path + '/' + name)),
+        );
       },
     );
   }
+}
+
+
+class FilePreview extends StatelessWidget {
+
+  FilePreview({this.path});
+
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    var body;
+
+    var mime = lookupMimeType(path);
+
+    if (mime == null || isTextFile(mime)) {
+      body = FutureBuilder<http.Response>(
+        future: http.get(path),
+        builder: (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data.body);
+          }
+          else if (snapshot.hasError) {
+            return Text("error");
+          }
+          else {
+            return Text(path);
+          }
+        },
+      );
+    }
+    else if (mime.startsWith("image/")) {
+      body = Image.network(path);
+    }
+    else {
+      body = Text("No preview available for mime type: " + mime);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(path + '/'),
+      ),
+      body: body,
+    );
+  }
+}
+
+bool isTextFile(String mime) {
+  return mime.startsWith("text/") ||
+    mime == "application/javascript" ||
+    mime == "application/json";
 }
